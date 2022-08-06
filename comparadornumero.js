0f4ac88b22d18a17c82196bbc1708b8a27716885
josetrivino89 <josetrivino89@gmail.com>
2022-08-06 12:43:52 -0300

    Ejercicio Java Adivinar numero

---
+++ comparadornumero.js
@@ -0,0 +1,4 @@
+let numero_para_comparar=prompt("ingrese un numero del 1 al 20 para jugar al adivinador")
+let numero_fijo= numero_para_comparar== 17 ? "ganaste! sergio de te debe un porron":"lo siento perdiste me debes un porron"
+
+console.log("tu resultado es: " + numero_fijo)
