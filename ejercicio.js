c787f525b1782279a28c041c02ebbc1a823080eb
josetrivino89 <josetrivino89@gmail.com>
2022-08-08 19:08:14 -0300

    practica js

---
+++ ejercicio.js
@@ -0,0 +1,29 @@
+// let distancia_km= prompt ("ingrese la cantidad de km")
+// let distancia_metros= distancia_km*1000
+ 
+// // console.log("la distancia en metros"+distancia_km+"km"+"es igual a"+distancia_metros)
+
+// console.log( `la distancia  de ${distancia_km} es igual a ${distancia_metros})
+
+
+// let altura=prompt("ingrese la altura en metros")
+// let peso=prompt ("ingrese el peso")
+
+// let inc= peso / (altura**2)
+
+// console.log (`el indice IMC  es de ${inc}`)
+
+// let distancia_km= prompt ("ingrese la cantidad de km")
+// let distancia_metros= distancia_km*1000
+
+// console.log("la distancia de "+ distancia_km+"km" + " es igual a "+ distancia_metros+" metros")
+
+// let edad=prompt("ingrese su edad")
+
+// let accese= edad <=18? "denegado":"permitido"
+// console.log (`su acceso esta` ${let accese})
+
+let numero_para_comparar=prompt("ingrese un numero del 1 al 20 para jugar al adivinador")
+let numero_fijo= numero_para_comparar== 17 ? "ganaste! sergio de te debe un porron":"lo siento perdiste me debes un porron"
+
+console.log("tu resultado es: " + numero_fijo)
